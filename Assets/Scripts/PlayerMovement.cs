26ee2ce5feb27798b7ef70422a5061f93613bd60
Vegi Woo <vegiwoo@protonmail.com>
2022-07-01 16:48:43 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start ()
+    {
+        m_Animator = GetComponent<Animator> ();
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_AudioSource = GetComponent<AudioSource> ();
+    }
+
+    void FixedUpdate ()
+    {
+        var horizontal = Input.GetAxis ("Horizontal");
+        var vertical = Input.GetAxis ("Vertical");
+        
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
+
+        var hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+        var hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        var isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool ("IsWalking", isWalking);
+        
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop ();
+        }
+        
+        var desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation (desiredForward);
+    }
+
+    void OnAnimatorMove ()
+    {
+        var velocity = m_Animator.deltaPosition.magnitude;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            velocity *= 2f;
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * velocity); 
+        m_Rigidbody.MoveRotation (m_Rotation);
+    }
+}
---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    [SerializeField, Range(0.5f,3.0f)] private float speed;
+    public Transform[] waypoints;
+
+    private int m_CurrentWaypointIndex;
+
+    private void Start ()
+    {
+        navMeshAgent.SetDestination (waypoints[0].position);
+        navMeshAgent.speed = speed;
+    }
+
+    private void Update ()
+    {
+        if (!(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)) return;
+        
+        m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+        navMeshAgent.SetDestination (waypoints[m_CurrentWaypointIndex].position);
+    }
+}
